ab3c7a7c3913510a8a187fee3a47a14f58a3275d
Darwin Gonzalez <darwinmanuel1@hotmail.com>
2025-05-16 17:03:58 -0400

    feat: add dayjs for time manipulation in TimeAgoPipe

--- src/app/domains/shared/pipes/time-ago.pipe.ts
+++ src/app/domains/shared/pipes/time-ago.pipe.ts
@@ -1,4 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 @Pipe({
   name: 'timeAgo',
@@ -6,35 +8,43 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimeAgoPipe implements PipeTransform {
   transform(value: Date | string): any {
-    const today = new Date();
-    const date = new Date(value);
+    // DAYSJS
+    dayjs.extend(relativeTime);
 
-    const diffYears = today.getFullYear() - date.getFullYear();
-    const diffMonths = today.getMonth() - date.getMonth();
-    const diffDays = today.getDay() - date.getDay();
-    const diffHours = today.getHours() - date.getHours();
-    const diffMinutes = today.getMinutes() - date.getMinutes();
+    return dayjs(value).fromNow();
+    // DAYSJS
 
-    if (diffYears > 0) {
-      return `${diffYears} month${diffYears > 1 ? 's' : ''} ago`;
-    }
+    /// PURE JAVASCRIPT
+    // const today = new Date();
+    // const date = new Date(value);
 
-    if (diffMonths > 0) {
-      return `${diffMonths} year${diffMonths > 1 ? 's' : ''} ago`;
-    }
+    // const diffYears = today.getFullYear() - date.getFullYear();
+    // const diffMonths = today.getMonth() - date.getMonth();
+    // const diffDays = today.getDay() - date.getDay();
+    // const diffHours = today.getHours() - date.getHours();
+    // const diffMinutes = today.getMinutes() - date.getMinutes();
 
-    if (diffDays > 0) {
-      return `${diffDays} day${diffDays > 1 ? 's' : ''} ago`;
-    }
+    // if (diffYears > 0) {
+    //   return `${diffYears} month${diffYears > 1 ? 's' : ''} ago`;
+    // }
 
-    if (diffHours > 0) {
-      return `${diffHours} hour${diffHours > 1 ? 's' : ''} ago`;
-    }
+    // if (diffMonths > 0) {
+    //   return `${diffMonths} year${diffMonths > 1 ? 's' : ''} ago`;
+    // }
 
-    if (diffMinutes > 0) {
-      return `${diffMinutes} minute${diffMinutes > 1 ? 's' : ''} ago`;
-    }
+    // if (diffDays > 0) {
+    //   return `${diffDays} day${diffDays > 1 ? 's' : ''} ago`;
+    // }
 
-    return 'just now';
+    // if (diffHours > 0) {
+    //   return `${diffHours} hour${diffHours > 1 ? 's' : ''} ago`;
+    // }
+
+    // if (diffMinutes > 0) {
+    //   return `${diffMinutes} minute${diffMinutes > 1 ? 's' : ''} ago`;
+    // }
+
+    // return 'just now';
+    /// PURE JAVASCRIPT
   }
 }
