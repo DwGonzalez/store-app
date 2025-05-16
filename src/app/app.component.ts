6cca38dc1226ca32da90242d9ca3bf949e741749
Darwin Gonzalez <darwinmanuel1@hotmail.com>
2025-05-16 17:56:18 -0400

    feat: create LayoutComponent with header and router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './domains/shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
-  template: `<app-header />
+  imports: [RouterOutlet],
+  template: `
     <div class="container mx-auto">
       <router-outlet />
-    </div> `,
+    </div>
+  `,
 })
 export class AppComponent {}
