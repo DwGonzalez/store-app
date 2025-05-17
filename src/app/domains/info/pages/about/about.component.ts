8e185a94a6af4b50f9191f7cd2574c556f7d744c
Darwin Gonzalez <darwinmanuel1@hotmail.com>
2025-05-16 20:02:19 -0400

    use new ng syntax with ng g `@angular/core:control-flow`

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CounterComponent } from '@shared/components/counter/counter.component';
-import { CommonModule } from '@angular/common';
+
 import { WaveAudioComponent } from '@/info/components/wave-audio/wave-audio.component';
 import { HighlightDirective } from '@shared/directives/highlight.directive';
 
@@ -9,10 +9,9 @@ import { HighlightDirective } from '@shared/directives/highlight.directive';
   standalone: true,
   imports: [
     CounterComponent,
-    CommonModule,
     WaveAudioComponent,
-    HighlightDirective,
-  ],
+    HighlightDirective
+],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
